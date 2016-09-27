c6371f5fd86dfab121bbd5b048320a1bfb5718a8
Brandon Liu <bdon@bdon.org>
2016-09-27 15:00:23 -0400

    add lambda function python file

---
+++ lambda_function.py
@@ -0,0 +1,25 @@
+import checks
+
+import requests
+import boto3
+import unittest
+
+client = boto3.client('sns')
+
+def lambda_handler(event,context):
+  loader = unittest.TestLoader()
+  suite = loader.loadTestsFromModule(checks)
+  result = unittest.TestResult()
+  suite.run(result)
+  print result
+  messages = result.errors + result.failures
+
+  if len(messages) > 0:
+    client.publish(
+        TopicArn=checks.SNS_TOPIC_ARN,
+        Message="\n".join(map(lambda m:m[1],messages)),
+        Subject='{0} tests failed'.format(len(messages))
+    )
+
+if __name__ == "__main__":
+  lambda_handler({},{})
